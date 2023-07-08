692dc59e739878589c1a47e682e33c6dd241e9e6
Aman Nanda <100416012+Aman1143@users.noreply.github.com>
2023-07-08 17:21:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,16 +43,17 @@ app.listen(80, () => {
 
 
 
-import { exec } from "child_process";
+import { spawn } from 'node:child_process';
+const ls = spawn('ls', ['-lh', '/usr']);
 
-exec("pwd", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
 });
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+}); 
