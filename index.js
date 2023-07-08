505cb3111097ff7e6e4c41af800036ec7a7ddd56
Aman Nanda <100416012+Aman1143@users.noreply.github.com>
2023-07-08 17:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ import path from "path";
 import os from "os"
 console.log(os.cpus());
 
+import { spawn } from 'node:child_process';
+const ls = spawn('pm2 list', ['-lh', '/usr']);
 
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+}); 
 
 
 app.use(cors());
@@ -43,17 +56,4 @@ app.listen(80, () => {
 
 
 
-import { spawn } from 'node:child_process';
-const ls = spawn('pm2 list');
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-}); 
